9efae24c5f0d8bfcfc54393957cd2ab1858dca19
Mischa Siekmann <mischa@gnumpi.com>
2026-01-21 19:45:16 +0100

    merged changes from esphome 2026.1.0

--- esphome/components/satellite1/satellite1.cpp
+++ esphome/components/satellite1/satellite1.cpp
@@ -24,11 +24,9 @@ void Satellite1::setup(){
 
 
 void Satellite1::dump_config(){
-  esph_log_config(TAG, "Satellite1 config:");
-  if( this->xmos_rst_pin_ ){
-    this->xmos_rst_pin_->dump_summary();
-  } else {
-    esph_log_config(TAG, "    xmos_rst_pin not set up properly.");
+  ESP_LOGCONFIG(TAG, "Satellite1 config:");
+  if( !this->xmos_rst_pin_ ){
+    ESP_LOGCONFIG(TAG, "    xmos_rst_pin not set up properly.");
   }
 }
 
