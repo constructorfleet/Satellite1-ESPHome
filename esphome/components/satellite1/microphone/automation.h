c28dfc54b03008a89665cc051a7c672c7a937e9b
Teagan Glenn <that@teagantotally.rocks>
2026-01-24 16:37:38 -0700

    add automation for pcm data trigger

---
+++ esphome/components/satellite1/microphone/automation.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "esphome/core/automation.h"
+#include "microphone.h"
+
+#include <vector>
+
+namespace esphome {
+namespace i2s_audio {
+
+class PCMDataTrigger : public Trigger<const std::vector<uint8_t> &> {
+ public:
+  explicit PCMDataTrigger(Microphone *mic) {
+    mic->add_pcm_data_callback([this](const std::vector<uint8_t> &data) { this->trigger(data); });
+  }
+};
+
+}  // namespace microphone
+}  // namespace esphome
