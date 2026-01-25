f8947577a522275f21a8d7932fdf3ee232ea2114
Teagan Glenn <that@teagantotally.rocks>
2026-01-24 23:54:52 -0700

    comma

--- esphome/components/satellite1/microphone/__init__.py
+++ esphome/components/satellite1/microphone/__init__.py
@@ -36,7 +36,7 @@ CONF_PDM = "pdm"
 CONF_ON_PCM_DATA = "on_pcm_data"
 PCMDataTrigger = i2s_audio_ns.class_(
     "PCMDataTrigger",
-    automation.Trigger.template(cg.int32.operator("const_ptr")m cg.size_t),
+    automation.Trigger.template(cg.int32.operator("const_ptr"), cg.size_t),
 )
 
 
